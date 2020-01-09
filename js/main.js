eb4a8a8ab9033f67c6a42f7912e575ad8ab0eef7
mkMolendijk <mark.molendijk@hotmail.com>
2020-01-09 11:42:56 +0100

    Added listener for button press

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let rAF = window.mozRequestAnimationFrame || window.requestAnimationFrame;
+const rAF = window.mozRequestAnimationFrame || window.requestAnimationFrame;
 let current = 0;
 let focusable = document.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
 
@@ -7,6 +7,18 @@ window.addEventListener('gamepadconnected', function(e) {
 });
 
 // event listener for vibration button
+document.querySelector('#btn-vibration').addEventListener('click', function (e) {
+    hapticFeedback();
+});
+
+function hapticFeedback() {
+    navigator.getGamepads()[0].vibrationActuator.playEffect('dual-rumble', {
+        startDelay: 0,
+        duration: 1000,
+        weakMagnitude: 0.5,
+        strongMagnitude: 1
+    });
+}
 
 function nextItem(index) {
     index++;
@@ -24,15 +36,6 @@ function clickItem(index) {
     if (focusable[index]) {
         focusable[index].click();
     }
-}
-
-function hapticFeedback(gp) {
-    gp.vibrationActuator.playEffect('dual-rumble', {
-        startDelay: 0,
-        duration: 1000,
-        weakMagnitude: 0.5,
-        strongMagnitude: 1
-    });
 }
 
 function updateLoop() {
